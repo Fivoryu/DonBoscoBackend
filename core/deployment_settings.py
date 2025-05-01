22d0fe70a0e8dc96682e617184391eaddfefb161
Fivoryu <trevorfelixcalerosuyo@gmail.com>
2025-05-01 19:03:58 -0400

    settings modificado 3

--- core/deployment_settings.py
+++ core/deployment_settings.py
@@ -25,6 +25,19 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173',  # Si usas Vite localmente
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
+
 STORAGES = {
   'default': {
     'BACKEND': 'django.core.files.storage.FileSystemStorage',
