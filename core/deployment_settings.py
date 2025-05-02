1e5f7c2bb94e49c32ef404045e37a9d164a89159
Fivoryu <trevorfelixcalerosuyo@gmail.com>
2025-05-02 12:07:33 -0400

    elimiando loggin

--- core/deployment_settings.py
+++ core/deployment_settings.py
@@ -54,16 +54,4 @@ DATABASES = {
     )
 }
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'root': {
-        'handlers': ['console'],
-        'level': 'DEBUG',
-    },
-}
+
